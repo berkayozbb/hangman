8a17f6806e269baf6e8fc19843dedf0f67a79b2c
Berkay <116227509+berkayozbb@users.noreply.github.com>
2023-02-03 17:49:25 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -57,10 +57,18 @@ data = [
   'Uzbekistan',
   'Vietnam',
 ]
+usedLetters=[]
+showScreen=False
 kelime=[]
 tahta=[]
+ipucu=["It's a country ;)"]
 import random
-secilenKelime=random.choice(data)
+for q in range(len(data)):
+    print("")
+if q <=56:
+    ipucu=ipucu[0]
+randNum=random.randrange(0,q)
+secilenKelime=data[randNum]
 secilenKelime=secilenKelime.lower()
 for i in range(len(secilenKelime)):
     harf=secilenKelime[i]
@@ -73,12 +81,15 @@ while case==True:
     if " _ " in tahta:
         print("")
     else:
+        print(f"Doğru Kelime : \n**********\n{secilenKelime.upper()}\n**********")
         print("Kelimeyi doğru bildiniz!\nTEBRİKLER")
         case=False
         break
     print(tahta)
     print(f"Kalan haklar : {kalanHak}")
     harf=input("İçinde olabileceğini düşündüğünüz harfi giriniz : \n(Yalnızca harf ve 1 karakter olmalı)\n")
+    usedLetters.append(harf)
+    print(f"Alınan harfler : \n{usedLetters}")
     if len(harf)>1:
         print("Yalnızca 1 karakter uzunluğunda harf giriniz!")
         continue
@@ -89,14 +100,23 @@ while case==True:
             tahta.insert(indexNo,harf)
         else:
             kalanHak-=1
+        if kalanHak<2 and kalanHak!=0 and showScreen==False:
+            ipucuSorgu=input("İpucu ister misin ?\n(Evet için : e )\n(Hayır için : h )")
+            if ipucuSorgu=="e":
+                print(f"\n**********\nCLUE : \n{ipucu}\n**********") 
+                showScreen=True   
         if kalanHak==0:
             print("Game Over :( ")
-            print(f"Doğru Kelime : \n**********\n{secilenKelime}\n**********")
+            print(f"Doğru Kelime : \n**********\n{secilenKelime.upper()}\n**********")
             break
     else:
         kalanHak-=1
+        if kalanHak<2 and kalanHak!=0 and showScreen==False:
+            ipucuSorgu=input("İpucu ister misin ?\n(Evet için : e )\n(Hayır için : h )")
+            if ipucuSorgu=="e":
+                print(f"\n**********\nCLUE : \n{ipucu}\n**********")
+                showScreen=True
         if kalanHak==0:
             print("Game Over :( ")
-            print(f"Doğru Kelime : \n**********\n{secilenKelime}\n**********")
+            print(f"Doğru Kelime : \n**********\n{secilenKelime.upper()}\n**********")
             break
-
